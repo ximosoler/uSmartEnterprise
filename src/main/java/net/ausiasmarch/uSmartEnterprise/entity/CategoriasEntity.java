d3a19030f68fe22c029c341405d33afc551c5765
Ximo Soler <xsoler98@gmail.com>
2022-12-20 20:19:11 +0100

    20deDic

--- src/main/java/net/ausiasmarch/uSmartEnterprise/entity/CategoriasEntity.java
+++ src/main/java/net/ausiasmarch/uSmartEnterprise/entity/CategoriasEntity.java
@@ -1,5 +1,60 @@
 package net.ausiasmarch.uSmartEnterprise.entity;
 
-public class CategoriasEntity {
-    
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "developer")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public class CategoriasEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @OneToMany(mappedBy = "categoria", fetch = FetchType.LAZY)
+    private final List<TareasEntity> tarea;
+
+    public CategoriasEntity() {
+        this.tarea = new ArrayList<>();
+    }
+
+    public CategoriasEntity(Long id) {
+        this.tarea = new ArrayList<>();
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTarea() {
+        return this.tarea.size();
+    }
+
 }
---
+++ src/main/java/net/ausiasmarch/uSmartEnterprise/entity/TareasEntity.java
@@ -0,0 +1,82 @@
+package net.ausiasmarch.uSmartEnterprise.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tareas")
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public class TareasEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long idUsuario;
+    private Long idCategoria;
+    private String nombre;
+    private String descripcion;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idCategoria")
+    private CategoriasEntity categorias;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Long idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public CategoriasEntity getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(CategoriasEntity categorias) {
+        this.categorias = categorias;
+    }
+
+}
